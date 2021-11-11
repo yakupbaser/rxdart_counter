ffb88dd52b066e4ff16c5cecf4aca51d5de97e64
yakupbaser <yakupbaser@hotmail.com.tr>
2021-11-11 09:08:41 +0300

    first init

---
+++ android/app/src/main/kotlin/com/cmos/rxdarttest/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cmos.rxdarttest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/counter_bloc.dart
@@ -0,0 +1,26 @@
+import 'package:rxdart/rxdart.dart';
+
+class CounterBloc {
+  int initialCount = 0;
+  BehaviorSubject<int>? _subjectCounter;
+
+  CounterBloc({required this.initialCount}) {
+    _subjectCounter = BehaviorSubject.seeded(initialCount);
+  }
+
+  ValueStream<int> get counterObservable => _subjectCounter!.stream;
+
+  void increment() {
+    initialCount++;
+    _subjectCounter!.sink.add(initialCount);
+  }
+
+  void decrement() {
+    initialCount++;
+    _subjectCounter!.sink.add(initialCount);
+  }
+
+  void dispose() {
+    _subjectCounter!.close();
+  }
+}
